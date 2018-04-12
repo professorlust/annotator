c986ebb1e5612015bf47dd252096cdbbf6513d88
sjyan <i@yanshengjia.com>
2018-04-12 18:09:38 +0800

    Complete billing screen function

--- static/js/billing.js
+++ static/js/billing.js
@@ -27,25 +27,20 @@ function screen(button) {
         contentType: false,
         cache: false,
         success: function(response) {
-            
-            var billing_table_title = document.getElementById('billing_table_title');
-            billing_table_title.innerHTML = document.getElementById("start_date").value+'-'+document.getElementById("end_date").value;
+            console.log("Response received.");
+            // var billing_table_title = document.getElementById('billing_table_title');
+            // billing_table_title.innerHTML = document.getElementById("start_date").value+'-'+document.getElementById("end_date").value;
             
             var billing_table = document.getElementById('billing_table');
-
-            //alert('Essay Grading Annotation:\n' + JSON.stringify(response['essay_progress']) + '\n\nOCR Result Annotation:\n' + JSON.stringify(response['ocr_progress']));
-            
             var td = billing_table.getElementsByTagName("td");
 
-            var i = 0;//annotator counter
-            for(var key in response['annotators']){
+            var i = 0;  //annotator counter
+            for(var key in response['annotators']) {
                td[i*3].innerHTML = response['essay_progress'][key];
                td[i*3+1].innerHTML = response['ocr_progress'][key];
                td[i*3+2].innerHTML = response['essay_progress'][key]+response['ocr_progress'][key];
                i++;
             }
-    
-            console.log(response);
         },
         error: function(jqXHR, textStatus, errorThrown)
         {
