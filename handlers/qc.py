6c8de66a12bf79b8a19fe72d5a2f245fb7fcd011
sjyan <i@yanshengjia.com>
2018-04-16 14:17:05 +0800

    Add logs

--- handlers/qc.py
+++ handlers/qc.py
@@ -9,6 +9,9 @@ from .base import *
 import calendar
 import datetime
 from datetime import date
+from numpy import NaN
+from sklearn.metrics import cohen_kappa_score
+
 
 class QCHandler(BaseHandler):
     @tornado.web.authenticated
@@ -35,10 +38,57 @@ class QCHandler(BaseHandler):
         timestamp = calendar.timegm(d.timetuple())
         return timestamp
 
+    def get_all_progress(self):
+        progress_essay_grading = []
+        essay_progress_collection = self.application.db.essay_progress
+        for account in self.application.accounts:
+            progress = essay_progress_collection.find_one({'annotator': account})['annotation_list']
+            progress_essay_grading.append(progress)
+
+        all_progress_essay_grading = set(progress_essay_grading[0])
+        for progress in progress_essay_grading:
+            all_progress_essay_grading = set(progress).intersection(all_progress_essay_grading)
+        return all_progress_essay_grading
+
     def calculate_qwk(self, start_date, end_date):
         start_timestamp = self.convert_date(start_date)
         end_timestamp = self.convert_date(end_date)
 
+
+
+        essay_data_db = db.essay_data
+        essay_data = {}
+        for essay_id in total_progress:
+            for data in essay_data_db.find({'essay_id':essay_id}):
+                data_id = str(data['_id'])
+                del data['_id']
+                essay_data[data_id] = data
+                pprint.pprint(data)
+        
+        # get y1 for cohen_kappa_score
+        y1 = []
+        for essay_id in total_progress:
+            for key in essay_data:
+                if essay_data[key]['essay_id'] == essay_id:
+                    y1.append(essay_data[key]['content_score'])
+                    y1.append(essay_data[key]['sentence_score'])
+                    y1.append(essay_data[key]['structure_score'])
+                    y1.append(essay_data[key]['vocabulary_score'])
+                    break
+            del essay_data[key]
+        
+        # get y2 for cohen_kappa_score
+        y2 = []
+        for key in essay_data:
+            y2.append(essay_data[key]['content_score'])
+            y2.append(essay_data[key]['sentence_score'])
+            y2.append(essay_data[key]['structure_score'])
+            y2.append(essay_data[key]['vocabulary_score'])
+
+        kappa_score = cohen_kappa_score(y1, y2, weights='quadratic')
+        kappa = {}
+        kappa['time'] = time.time()
+        kappa['kappa'] = kappa_score
 
 
         qwk = 0.0
