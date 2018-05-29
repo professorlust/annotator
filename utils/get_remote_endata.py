28b4e005bd6a71e60a45b416cc997160bf75fb29
sjyan <i@yanshengjia.com>
2018-05-29 10:53:54 +0800

    Add essay_scandata.txt

--- utils/get_remote_endata.py
+++ utils/get_remote_endata.py
@@ -5,6 +5,7 @@
 # @email: i@yanshengjia.com
 # Copyright @ Shengjia Yan. All Rights Reserved.
 
+import codecs
 import json
 from bson import json_util
 import pymongo
@@ -47,30 +48,42 @@ def get_items_info(ip, port):
                     ofile.write(prompt_json + '\n')
 
 def get_essay_scandata(ip, port):
-    # conn = MongoClient(ip, port)
-    # db = conn.klx_analysis
-    # temp_endata = db.temp_endata
+    conn = MongoClient(ip, port)
+    db = conn.klx_analysis
+    temp_endata = db.temp_endata
     valid_item_id_list = []     # item id with prompt info
 
-    with open(essay_items_info_filepath, 'r') as ifile:
+    with codecs.open(essay_items_info_filepath, mode='r', encoding='ascii', errors='ignore') as ifile:
         for line in ifile:
             line = line.strip()
             item_info = json.loads(line)
             if item_info['stem_plain'] != '':
                 valid_item_id_list.append(item_info['item_id'])
 
-    print(valid_item_id_list)
+    with open(essay_scandata_filepath, 'a') as ofile:
+        ofile.seek(0)
+        ofile.truncate()
+        for item in temp_endata.find():
+            paper_id    = item['paper_id']
+            answer_data = item['answer_data']
+            for answer in answer_data:
+                answer_data = {}
+                item_id = answer['item_id']
+                if item_id in valid_item_id_list:
+                    image_url   = answer['url']
+                    total_score = answer['total_score']
+                    score       = answer['score']
+                    number      = answer['number']
 
-    # with open(essay_scandata_filepath, 'a') as ofile:
-    #     ofile.seek(0)
-    #     ofile.truncate()
-    #     for item in temp_endata.find():
-    #         answer_data = item['answer_data']
+                    answer_data['paper_id']    = paper_id
+                    answer_data['item_id']     = item_id
+                    answer_data['image_url']   = image_url
+                    answer_data['total_score'] = total_score
+                    answer_data['score']       = score
+                    answer_data['number']      = number
 
-
-
-
-
+                    answer_json = json.dumps(answer_data)
+                    ofile.write(answer_json + '\n')
 
 
 def main():
