b32dcbc2b89afc49d4330a10ec37679dd5fe2c90
sjyan <i@yanshengjia.com>
2018-07-04 18:23:38 +0800

    Add save2csv function

--- utils/download_essay_images.py
+++ utils/download_essay_images.py
@@ -6,13 +6,16 @@
 # Copyright @ Shengjia Yan. All Rights Reserved.
 
 import os
+import csv
 import json
+import codecs
 
 ocr_data_path = '../data/ocr/batch_0/full_essay_data.txt'
 save_dir = '../data/img/'
+samples_path = '../data/samples.csv'
 
 def download_images(quantity=2000):
-    with open(ocr_data_path, 'r') as in_file:
+    with codecs.open(ocr_data_path, 'r') as in_file:
         counter = 0
         for line in in_file:
             counter += 1
@@ -35,7 +38,7 @@ def download_images(quantity=2000):
                 o_file.write(essay)
 
 def get_essay_statistics():
-    with open(ocr_data_path, 'r') as in_file:
+    with codecs.open(ocr_data_path, 'r') as in_file:
         counter = 0
         max = 21
         scores = [0] * max
@@ -50,29 +53,37 @@ def get_essay_statistics():
 
 def uniform_sampling():
     quota = [150] * 16
-    with open(ocr_data_path, 'r') as in_file:
+    samples = []
+    with codecs.open(ocr_data_path, 'r') as in_file:
         counter = 0
         for line in in_file:
-            counter += 1
+            essay_dict = {}
             info = json.loads(line)
             score = int(info['score'])
 
-            if (quota[score] <= 0):
+            if (score > 15 or quota[score] <= 0):
                 continue
-
+            
+            counter += 1
             image_url = info['image_url']
             essay = info['ocr_correction']
             image_id = info['image_id']
             image_name = image_id.replace('.jpg', '')
-            fname = str(score) + '_' + image_name
-            essay_fname = save_dir + fname + '.txt'
+            essay_dict['number'] = counter
+            essay_dict['id'] = image_name
+            essay_dict['essay'] = essay
+            samples.append(essay_dict)
+            fname = str(counter) + '_' + image_name
             image_fname = save_dir + fname + '.jpg'
             os.system('wget ' + image_url + ' -O ' + image_fname)
-            os.system('touch ' + essay_fname)
-            with open(essay_fname, 'w') as o_file:
-                o_file.write(essay)
             
             quota[score] -= 1
+    
+    with open(samples_path, mode='w', encoding="utf8", errors='ignore') as out_file:
+        writer = csv.DictWriter(out_file, samples[0].keys())
+        writer.writeheader()
+        for row in samples:
+            writer.writerow(row)
 
 def main():
     uniform_sampling()
